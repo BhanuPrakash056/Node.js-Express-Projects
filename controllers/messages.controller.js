8c4266617f2f170d29523374899525be8ad160ec
bhanu prakash <r19cs056@cit.reva.edu.in>
2023-09-03 20:07:40 +0530

    sending file/ images

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,11 @@
-function getMessages(req, res) {
-  res.send("<ul><li>Helloo Albert!</li></ul>");
+const path = require('path')
+function getMessages(req,res) {
+  // res.send("<ul><li>Helloo Albert!</li></ul>");
+  
+  res.sendFile(path.join(__dirname,'..','public','skimountain.jpg'))
 }
 
-function postMessage(req, res) {
+function postMessage(req,res) {
   res.send("updated messages");
 }
 
